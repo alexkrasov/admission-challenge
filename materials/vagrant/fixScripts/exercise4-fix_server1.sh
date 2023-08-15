070a78f1ebc6819a74f2cf6e04c4b60b52f6e1c1
krasov_alexander <krasov.alexander@gmail.com>
2023-08-15 23:07:10 +0300

    Exercise 4 fix

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
 #add fix to exercise4-server1 here
+echo "192.168.60.11 server2" | sudo tee -a /etc/hosts
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo service ssh restart
--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
 #add fix to exercise4-server2 here
+echo "192.168.60.10 server1" | sudo tee -a /etc/hosts
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo service ssh restart
